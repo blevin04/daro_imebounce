761a6edd76380d9a0f9d38344225ca1032c5190f
blevin04 <jamestonito04@gmail.com>
2024-10-17 08:39:13 +0300

    Initial push

---
+++ android/app/src/main/kotlin/com/example/class_occupation_system/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.class_occupation_system
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/roomsdata.dart
@@ -0,0 +1,12 @@
+Map<String, dynamic> buildings = {
+  "Resource center": {
+    "Ground Flow": {
+      "Room name": {
+        "Location": "1239482",
+        "type": "class/lab/office",
+        "occupants": ["list of people in the office or room"],
+        "vacant": "true/false"
+      }
+    }
+  }
+};

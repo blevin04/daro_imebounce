7db4ac14cce30b67fc58db236781c2211a1902d0
blevin04 <jamestonito04@gmail.com>
2024-10-17 09:39:51 +0300

    location added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -7,11 +8,104 @@ class Homepage extends StatefulWidget {
   State<Homepage> createState() => _HomepageState();
 }
 
+late LocationData _locationData;
+Future getlocation() async {
+  Location location = new Location();
+
+  bool _serviceEnabled;
+  PermissionStatus _permissionGranted;
+
+  _serviceEnabled = await location.serviceEnabled();
+  if (!_serviceEnabled) {
+    _serviceEnabled = await location.requestService();
+    if (!_serviceEnabled) {
+      return location;
+    }
+  }
+  _permissionGranted = await location.hasPermission();
+  if (_permissionGranted == PermissionStatus.denied) {
+    _permissionGranted = await location.requestPermission();
+    if (_permissionGranted != PermissionStatus.granted) {
+      return;
+    }
+  }
+
+  _locationData = await location.getLocation();
+}
+
 class _HomepageState extends State<Homepage> {
   @override
+  void initState() {
+    // TODO: implement initState
+    getlocation();
+    Location.instance.onLocationChanged.listen((LocationData currentlocation) {
+      _locationData = currentlocation;
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // getlocation();
     return Scaffold(
       appBar: AppBar(),
+      body: FutureBuilder(
+        future: getlocation(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return StreamBuilder(
+            stream: Location.instance.onLocationChanged,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              Location.instance.changeSettings(
+                  accuracy: LocationAccuracy.high, interval: 100);
+              String latitude = snapshot.data.latitude.toString();
+              String longitude = snapshot.data.longitude.toString();
+              String altitude = snapshot.data.altitude.toString();
+              return Center(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        const Text(
+                          "latitude",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        Text(
+                          latitude,
+                          style: const TextStyle(color: Colors.black),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        const Text(
+                          "longitude",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        Text(
+                          longitude,
+                          style: const TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [Text("altitude"), Text(altitude)],
+                    )
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
